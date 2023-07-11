ef1998aff1e1cb2887ef1316c7f987ac81896464
davidcreates265 <davidcreatesmw@gmail.com>
2023-07-11 16:20:29 +0200

    Changed procfile and modified port

--- app.js
+++ app.js
@@ -337,6 +337,11 @@ app.get('/download-report', (req, res) => {
 
 
 
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
 
-
-app.listen(process.env.PORT || 3000, () => console.log('Server has started'));
+app.listen(port, function() {
+  console.log("Server has started successfully");
+});
