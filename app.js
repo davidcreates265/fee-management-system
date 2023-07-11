f93fb47af6e5fea52679a00f518943d171034042
davidcreates265 <davidcreatesmw@gmail.com>
2023-07-11 17:15:24 +0200

    merged user-functions

--- app.js
+++ app.js
@@ -336,7 +336,10 @@ app.get('/download-report', (req, res) => {
 });
 
 
-app.listen(3000, () => console.log('Server started on port 3000'));
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
 
 app.listen(port, function() {
   console.log("Server has started successfully");
