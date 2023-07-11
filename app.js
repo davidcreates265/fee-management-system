fff888860f3ec0ee829c32480b3de73de0fb31d9
davidcreates265 <davidcreatesmw@gmail.com>
2023-07-11 16:51:06 +0200

    Fixing bugs - branch master

--- app.js
+++ app.js
@@ -145,4 +145,11 @@ app.get('/create-user', function(req, res) {
 });
 
 
-app.listen(3000, () => console.log('Server started on port 3000'));
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
+
+app.listen(port, function() {
+  console.log("Server has started successfully");
+});
